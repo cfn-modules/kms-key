d84571f84571e7177b11f75814ee9d713c7c0db9
Michael Wittig <michael@widdix.de>
2019-08-30 14:53:15 +0200

    fix test

--- test/alias-name.js
+++ test/alias-name.js
@@ -4,7 +4,7 @@ const cfntest = require('@cfn-modules/test');
 test.serial('alias-name', async t => {
   const stackName = cfntest.stackName();
   try {
-    t.log(await cfntest.createStack(`${__dirname}/alias-name.yml`, stackName, {}));
+    t.log(await cfntest.createStack(`${__dirname}/alias-name.yml`, stackName, {AliasName: stackName}));
     // what could we test here?
   } finally {
     t.log(await cfntest.deleteStack(stackName));
