b75cf128498e02befc3f23e735f1f9e3de1308b8
Michael Wittig <michael@widdix.de>
2019-08-30 13:09:44 +0200

    fix test

--- test/iam-access.js
+++ test/iam-access.js
@@ -1,10 +1,16 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
+const AWS = require('aws-sdk');
 
 test.serial('read', async t => {
+  const sts = new AWS.STS({apiVersion: '2011-06-15'});
+  const identity = await sts.getCallerIdentity({}).promise();
   const stackName = cfntest.stackName();
   try {
-    t.log(await cfntest.createStack(`${__dirname}/iam-access-with-admin.yml`, stackName, {IamAccess: 'Read'}));
+    t.log(await cfntest.createStack(`${__dirname}/iam-access-with-admin.yml`, stackName, {
+      IamAccess: 'Read',
+      IamAdminAccess: identity.Arn
+    }));
     // what could we test here?
   } finally {
     t.log(await cfntest.deleteStack(stackName));
@@ -13,9 +19,14 @@ test.serial('read', async t => {
 });
 
 test.serial('use', async t => {
+  const sts = new AWS.STS({apiVersion: '2011-06-15'});
+  const identity = await sts.getCallerIdentity({}).promise();
   const stackName = cfntest.stackName();
   try {
-    t.log(await cfntest.createStack(`${__dirname}/iam-access-with-admin.yml`, stackName, {IamAccess: 'Use'}));
+    t.log(await cfntest.createStack(`${__dirname}/iam-access-with-admin.yml`, stackName, {
+      IamAccess: 'Use',
+      IamAdminAccess: identity.Arn
+    }));
     // what could we test here?
   } finally {
     t.log(await cfntest.deleteStack(stackName));
